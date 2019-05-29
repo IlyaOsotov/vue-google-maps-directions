27cc6f4722348ea2b077f932086443e3ffa908df
IlyaOsotov <soten58@yandex.ru>
2019-05-29 11:12:26 +0300

    added direcrionsService

--- src/components/googleMap.vue
+++ src/components/googleMap.vue
@@ -1,6 +1,7 @@
 <template>
    <div>
-     <GmapMap
+    <GmapMap
+      ref="mapRef"
       :center="{lat:55.7492, lng:37.6161}"
       :zoom="14"
       class="google-map"
@@ -9,14 +10,38 @@
 </template>
 
 <script>
+import { gmapApi } from 'vue2-google-maps';
+
 export default {
   name: 'google-map',
   data() {
     return {
     };
   },
-  computed: {},
-  methods: {},
+  computed: {
+    google: gmapApi,
+  },
+  methods: {
+    calculateAndDisplayRoute(directionsDisplay, directionsService) {
+      directionsService.route({
+        origin: 'Башня Федерация, Москва',
+        destination: 'Красная площадь, Москва',
+        travelMode: 'WALKING',
+      }, (response, status) => {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+        }
+      });
+    },
+  },
+  mounted() {
+    this.$refs.mapRef.$mapPromise.then((map) => {
+      const directionsService = new this.google.maps.DirectionsService();
+      const directionsDisplay = new this.google.maps.DirectionsRenderer({ map });
+
+      this.calculateAndDisplayRoute(directionsDisplay, directionsService);
+    });
+  },
 };
 </script>
 
