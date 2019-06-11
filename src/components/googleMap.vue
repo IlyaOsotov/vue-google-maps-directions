c102617c7945216c9cf6400116a0cf65f41b4bc6
IlyaOsotov <soten58@yandex.ru>
2019-06-11 16:48:03 +0300

    added actions with citiesList

--- src/components/googleMap.vue
+++ src/components/googleMap.vue
@@ -6,13 +6,18 @@
       :zoom="14"
       class="google-map"
     ></GmapMap>
+    <input @keyup.enter="addCity" v-model="newCity">
+    <button @click="addCity">Добавить точку</button>
     <draggable
       class="list-group"
       v-model="citiesList"
       group="cities"
       @start="drag=true"
       @end="drag=false">
-      <div class="list-group-item" v-for="element in citiesList" :key="element">{{element}}</div>
+      <div class="list-group-item-container" v-for="element in citiesList" :key="element">
+        <div class="inline list-group-item">{{element}}</div>
+        <button class="inline remove" @click="removeCity(element)">x</button>
+      </div>
     </draggable>
    </div>
 </template>
@@ -26,6 +31,7 @@ export default {
   data() {
     return {
       citiesList: ['Башня Федерация', 'Красная площадь'],
+      newCity: '',
     };
   },
   components: {
@@ -35,6 +41,13 @@ export default {
     google: gmapApi,
   },
   methods: {
+    addCity() {
+      this.citiesList.push(this.newCity);
+      this.newCity = '';
+    },
+    removeCity(element) {
+      this.citiesList = this.citiesList.filter(el => el !== element);
+    },
     calculateAndDisplayRoute(directionsDisplay, directionsService) {
       const { 0: first, [this.citiesList.length - 1]: last } = this.citiesList;
       directionsService.route({
@@ -60,6 +73,28 @@ export default {
 </script>
 
 <style scoped>
+button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  margin: 0 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  height: 45px;
+}
+input {
+  height: 40px;
+}
+.inline {
+  display: inline-block;
+}
+.remove {
+  background-color: coral;
+  padding: 15px 25px;
+}
 .map-container {
   display: flex;
   flex-direction: row;
@@ -72,14 +107,17 @@ export default {
   margin-bottom: 0;
   min-height: 20px;
 }
+.list-group-item-container {
+  margin: 5px 0;
+}
 .list-group-item {
   cursor: move;
   position: relative;
-  display: block;
   padding: .75rem 1.25rem;
   margin-bottom: -1px;
   background-color: #fff;
   border: 1px solid rgba(0,0,0,.125);
+  width: 200px;
 }
 .google-map {
   width: 640px;
